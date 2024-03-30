d7cea898869b059f2f4f9362a99a4c0337817c22
samir <106090175+snusav1j@users.noreply.github.com>
2024-03-30 06:26:30 +0300

    Add files via upload

--- modal/task_info_modal.js
+++ modal/task_info_modal.js
@@ -1,16 +1,18 @@
 const modal = document.getElementById('myModal');
 
-tasksList.addEventListener('dblclick', openModal);
+tasksList.addEventListener('dblclick', openTaskModal);
 window.addEventListener('click', closeModalOut)
 
-function openModal(e){
+function openTaskModal(e){
     const taskModal = e.target.closest('li.list-group-item');
 
     // узнать статус задачи по id
     const taskStatus = taskModal.closest('ul').id
     let status = '';
+    let statusClass = '';
     if (taskStatus == 'tasksList'){
         status = 'Задача в работе';
+        statusClass = 'in-process'
     }
 
     // найти по индексу с помощью id 
@@ -18,34 +20,57 @@ function openModal(e){
     const index = tasks.findIndex((task) => task.id === id);
 
     let modalInfo = 
-    `<div class="modal-content">
-        
+    `<div id="${id}" class="modal-content">
+    
+    <span class="close-modal-btn"><img src="img/close.svg" alt="Done" width="28" height="28"></span>
         <div class="modal-content-header">
-            <span class="modal-task-number">Задача номер: ${id} (${index+1})</span>
-            <span class="modal-task-text">${tasks[index].text}</span>
-            <span class="modal-task-date">Создано ${tasks[index].addDate} в ${tasks[index].addTime}</span>
-            <span class="modal-task-info">Статус задачи: ${status}</span>
-            <span class="close-modal-btn"><img src="img/close.svg" alt="Done" width="24" height="24"></span>
+            <span class="modal-task-id">Задача №${id}</span>
         </div>
+        <div class="modal-task-info">
+            <span class="modal-task-num">Информация о ${index+1}-ой задаче в списке:</span>
+            <span class="modal-task-text">Текст задачи: ${tasks[index].text}</span>
+            <span class="modal-task-date">Создано ${tasks[index].addDate} в ${tasks[index].addTime}</span>
+            <span class="modal-task-status ${statusClass}">Статус задачи: ${status}</span>
+        </div>
+        
     </div>
     `;
-    modal.innerHTML = modalInfo;
-    modal.style.display = "block";
 
+    modal.innerHTML = modalInfo;
+    modalContent = document.getElementsByClassName('modal-content')[0];
+    
+    //появление модального с классом
+    modal.style.display = "block";
+    modalContent.classList.add('show-modal');
+    
+    // кнопка закрыть модальное окно
     closeBtn = document.getElementsByClassName('close-modal-btn')[0];
     closeBtn.addEventListener('click', closeModal)
 }
 
 function closeModal() {
     modalContent = document.getElementsByClassName('modal-content')[0];
-    modal.style.display = "none";
-    modalContent.remove();
+    
+    modalContent.classList.remove('show-modal');
+    modalContent.classList.add('hide-modal');
+
+    setTimeout(() => {
+        modal.style.display = "none";
+        modalContent.remove();
+    }, 400)
+    
 }
 
 function closeModalOut(e) {
     if (e.target == modal) {
         modalContent = document.getElementsByClassName('modal-content')[0];
-        modalContent.remove();
-        modal.style.display = "none";
+
+        modalContent.classList.remove('show-modal');
+        modalContent.classList.add('hide-modal');
+    
+        setTimeout(() => {
+            modal.style.display = "none";
+            modalContent.remove();
+        }, 400)
     }
 }
